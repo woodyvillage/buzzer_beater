5e32b2d90b9800ce754e61a259834b10c8f37d59
woodyvillage <com.woodyvillage@gmail.com>
2020-10-22 20:13:33 +0900

    update

--- lib/dto/setting.dart
+++ lib/dto/setting.dart
@@ -10,4 +10,18 @@ class SettingDto {
   String notfoul;
   String play;
   String change;
+
+  SettingDto({
+    this.fieldGoal,
+    this.freeThrow,
+    this.ownGoal,
+    this.periodend,
+    this.timeout,
+    this.nottimeout,
+    this.teamfoul,
+    this.notteamfoul,
+    this.notfoul,
+    this.play,
+    this.change,
+  });
 }
